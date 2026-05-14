b25e5c4a38bd44e830d7c1db07aa9e9cb7046a8d
tasniatropa420-jpg <tasniatropa420@gmail.com>
2026-05-14 14:39:38 +0000

    Binary Search.C

---
+++ Binary Search.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {5, 10, 15, 20, 25, 30};
+    int n = 6;
+    int key = 20;
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
