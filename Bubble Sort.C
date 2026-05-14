0f2c151d0f21cd32f00b2ef6b0ee576a59cad72e
tasniatropa420-jpg <tasniatropa420@gmail.com>
2026-05-14 14:55:27 +0000

    Bubble Sort.C

---
+++ Bubble Sort.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap the elements
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
