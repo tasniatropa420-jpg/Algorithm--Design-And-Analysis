84b9dd0418dd18233b6df7788187f0c533325dcb
tasniatropa420-jpg <tasniatropa420@gmail.com>
2026-05-14 20:56:36 +0600

    Update Bubble Sort.C

--- Bubble Sort.C
+++ Bubble Sort.C
@@ -19,7 +19,7 @@ int main() {
     
     bubbleSort(arr, n);
     
-    printf("Sorted array: ");
+    printf("Sorted array: 11");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
